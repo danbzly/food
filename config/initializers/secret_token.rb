81142d4a47f59fcb04aa096c80b1d1ebc9054efe
Daniel Beasley <danbzly@gmail.com>
2013-07-03 10:51:47 -0400

    commited files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Food::Application.config.secret_token = '4b348e35766ef93ac95c15a28615258a6e2db5e12d9f01f3785448c48dc0ffe477c23b6f86fe79941b5ee07c9fc4fe3a8184f933becb44f3b4faa40061a83b8f'
